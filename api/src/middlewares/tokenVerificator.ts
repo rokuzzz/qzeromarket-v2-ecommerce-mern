96c469a1942a4f343944868f24121cd2acd202a5
Roman Kuzero <rombeldan@gmail.com>
2023-02-13 17:57:58 +0200

    complete verifyTokenAndAuthorization middleware to build updateUser method

---
+++ api/src/middlewares/tokenVerificator.ts
@@ -0,0 +1,49 @@
+import { Express, NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../util/secrets";
+import { ForbiddenError, UnauthorizedError } from "../helpers/apiError";
+import User, { UserDocument } from "../models/User";
+
+// export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+//   const authHeader = req.headers.authorization
+//   if(authHeader){
+//     const token = (<string>authHeader).split(' ')[1]
+//     jwt.verify(token, JWT_SECRET, (err: any, decoded: any) => {
+//       if(err) throw new ForbiddenError()
+//       req.user = decoded
+//       next()
+//     })
+//   }else{
+//     throw new UnauthorizedError()
+//   }
+// }
+
+// export const verifyTokenAndAuthorization = (req: Request, res: Response, next: NextFunction) => {
+//   verifyToken(req, res, () => {
+//     const user = new User
+//     if(req.user?.id === req.params.id || req.user?.role === 'admin'){
+//       next()
+//     } else {
+//       throw new ForbiddenError()
+//     }
+//   })
+// }
+
+export const verifyTokenAndAuthorization = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization
+  if (authHeader) {
+    const token = (<string>authHeader).split(" ")[1]
+    jwt.verify(token, JWT_SECRET, (err: any, decoded: any) => {
+      if (err) throw new ForbiddenError()
+      else {
+        req.user = decoded
+        if(decoded.role == 'admin' || decoded.id == req.params.id) next()
+        else {
+          throw new ForbiddenError('Access is denied! You must be an administrator to change user data.')
+        }
+      }
+    })
+  } else {
+    throw new UnauthorizedError('You are not authorized!')
+  }
+}
--- api/src/routes/userRoute.ts
+++ api/src/routes/userRoute.ts
@@ -1,8 +1,30 @@
 import { Router, Request, Response } from "express";
+import CryptoJS from 'crypto-js'
+
+import { verifyTokenAndAuthorization } from "../middlewares/tokenVerificator";
 import userController from "../controllers/userController";
+import { CRYPTO_SECRET } from "../util/secrets";
+import User from "../models/User";
 
 const userRoute = Router()
 
 userRoute.get('/', userController.getAllUsers)
+userRoute.put('/:id', verifyTokenAndAuthorization, async (req: Request, res: Response) => {
+  if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      CRYPTO_SECRET
+    ).toString()
+  }
+
+  try{
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, {new: true})
+    res.status(200).json(updatedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 export default userRoute
