ff44105c71d501f231d9e827420802ea9447c168
Roman Kuzero <rombeldan@gmail.com>
2023-02-20 17:13:07 +0200

    start building product crud operations - createProduct; build getIdsByNames in categoryService ge for createProduct

--- api/src/controllers/productController.ts
+++ api/src/controllers/productController.ts
@@ -1,27 +1,30 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 import productService from "../services/productService";
 import categoryService from '../services/categoryService';
 import Product from "../models/Product";
-import { NotFoundError } from "../helpers/apiError";
 
 const createProduct = async (req: Request, res: Response) => {
   const {title, description, price, categories, image} = req.body
   const categoryIds = await categoryService.getIdsByNames(categories)
 
-  const newProduct = new Product({
-    title: title,
-    description: description,
-    price: price,
-    categories: categoryIds,
-    image: image
-  })
-  
-  try {
-    const savedProduct = await productService.createOne(newProduct)
-    res.status(200).json(savedProduct)
-  } catch (err) {
-    res.status(500).json(err)
+  if(categoryIds.length == 0) {
+    res.status(404).json('The categories field is empty or were entered incorrectly')
+  } else {
+    const newProduct = new Product({
+      title: title,
+      description: description,
+      price: price,
+      categories: categoryIds,
+      image: image
+    })
+    
+    try {
+      const savedProduct = await productService.createOne(newProduct)
+      res.status(200).json(savedProduct)
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 }
 
