17248bcb35785f7c59ad27dbd181a079dab3332a
Roman Kuzero <rombeldan@gmail.com>
2023-03-17 15:39:12 +0200

    Replace res.status(500).json(err) to next(err) in catch block in categoryController.ts to send the right status code if catches error.

--- api/src/controllers/categoryController.ts
+++ api/src/controllers/categoryController.ts
@@ -1,36 +1,36 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import categoryService from "../services/categoryService";
 import Category from "../models/Category";
 
-const createCategory = async (req: Request, res: Response) => {
+const createCategory = async (req: Request, res: Response, next: NextFunction) => {
   const newCategory = new Category(req.body)
   try{
     const savedCategory = await categoryService.createOne(newCategory)
     res.status(200).send(savedCategory)
   } catch (err) {
-    res.status(500).json(err)
+    next(err)
   }
 }
 
-const getAllCategories = async (req: Request, res: Response) => {
+const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
   try{
     const categories = await categoryService.findAll()
     res.status(200).json(categories)
   } catch (err) {
-    res.status(500).json(err)
+    next(err)
   }
 }
 
-const getCategoryById = async (req: Request, res: Response) => {
+const getCategoryById = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category = await categoryService.findById(req.params.id)
     res.status(200).json(category)
   } catch (err) {
-    res.status(500).json(err)
+    next(err)
   }
 }
 
-const updateCategory = async (req: Request, res: Response) => {
+const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category = await categoryService.updateOne(
       req.params.id, 
@@ -39,16 +39,16 @@ const updateCategory = async (req: Request, res: Response) => {
     )
     return res.status(201).json(category);
   } catch (err) {
-    res.status(500).json(err)
+    next(err)
   }
 }
 
-const deleteCategory = async (req: Request, res: Response) => {
+const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
   try {
     await categoryService.deleteOne(req.params.id)
     res.status(200).json('Category has been deleted.')
   } catch (err) {
-    res.status(500).json(err)
+    next(err)
   }
 }
 
