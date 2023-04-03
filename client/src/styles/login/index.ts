d4f713ca7786baf2d64b4ea78370add9332f6390
Roman Kuzero <rombeldan@gmail.com>
2023-04-03 15:33:18 +0300

    Customize boxShadow property for LoginBox: set the boxShadow property to none for small screens, choose an appropriate value for other screen sizes.

--- client/src/styles/login/index.ts
+++ client/src/styles/login/index.ts
@@ -7,7 +7,12 @@ export const LoginWrapper = styled(Grid)(({ theme }) => ({
 }));
 
 export const LoginBox = styled(Paper)(({ theme }) => ({
+  boxSizing: 'border-box',
   padding: theme.spacing(10, 0),
   textAlign: 'center',
   color: theme.palette.text.secondary,
+  boxShadow: 'rgb(38, 57, 77) 0px 20px 30px -10px',
+  [theme.breakpoints.down('sm')]: {
+    boxShadow: 'none'
+  },
 }));
