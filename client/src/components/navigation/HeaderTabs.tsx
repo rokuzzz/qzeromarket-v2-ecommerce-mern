45fdbf01e5fd80c124bbf75176c0aedf470ca74d
Roman Kuzero <rombeldan@gmail.com>
2023-04-20 22:44:53 +0300

    feat (HeaderTabs.tsx): Make tabs scrollable and responsive - Set tabs to be scrollable when there are too many to fit in the viewport - Adjust tab size based on screen size for better responsiveness

--- client/src/components/navigation/HeaderTabs.tsx
+++ client/src/components/navigation/HeaderTabs.tsx
@@ -1,4 +1,4 @@
-import { Tab, Tabs } from '@mui/material';
+import { Box, Tab, TabScrollButton, Tabs } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 interface HeaderTabsProps {
@@ -8,7 +8,7 @@ interface HeaderTabsProps {
 
 const StyledTab = styled(Tab)(({ theme }) => ({
   [theme.breakpoints.up('sm')]: {
-    width: '125px'
+    width: '125px',
   },
 }));
 
@@ -16,22 +16,23 @@ const HeaderTabs = (Props: HeaderTabsProps) => {
   const { value, onChange } = Props;
 
   return (
-    <Tabs
-      value={value}
-      onChange={onChange}
-      textColor={'inherit'}
-      indicatorColor='secondary'
-      aria-label='main categories'
-      sx={{
-        margin: '0 auto',
-      }}
-    >
-      <StyledTab label='All' />
-      <StyledTab label='New in' />
-      <StyledTab label='Clothing' />
-      <StyledTab label='Shoes' />
-      <StyledTab label='Accessories' />
-    </Tabs>
+    <Box sx={{ maxWidth: { xs: '95%', sm: 480 }, margin: '0 auto' }}>
+      <Tabs
+        value={value}
+        onChange={onChange}
+        textColor={'inherit'}
+        indicatorColor='secondary'
+        aria-label='main categories'
+        variant='scrollable'
+        scrollButtons='auto'
+      >
+        <StyledTab label='All' />
+        <StyledTab label='New in' />
+        <StyledTab label='Clothing' />
+        <StyledTab label='Shoes' />
+        <StyledTab label='Accessories' />
+      </Tabs>
+    </Box>
   );
 };
 
