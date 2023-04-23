9d3e6e88e088f9a61188defd300ccd5d93d6ed68
Roman Kuzero <rombeldan@gmail.com>
2023-04-23 18:38:20 +0300

    style (ProductDetails.tsx): Style the ProductDetails page - Add makeStyles hook to style the components - Display product image and details side by side using Grid and Box components - Ensure responsive design for various screen sizes

--- client/src/pages/ProductDetails.tsx
+++ client/src/pages/ProductDetails.tsx
@@ -1,9 +1,66 @@
+import { makeStyles } from '@mui/styles';
+import {
+  Theme,
+  Box,
+  Grid,
+  Paper,
+  Typography,
+  Button,
+  Toolbar,
+} from '@mui/material';
 import { useParams } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../hooks/appHooks';
 import { useEffect } from 'react';
+
+import { useAppDispatch, useAppSelector } from '../hooks/appHooks';
 import { getProductByID } from '../redux/slices/productSlice';
+import Header from '../components/navigation/Header';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    margin: theme.spacing(2),
+    height: '100%',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+  },
+  imageWrapper: {
+    height: '100%',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: theme.spacing(2),
+  },
+  title: {
+    fontWeight: 700,
+    marginBottom: theme.spacing(5),
+  },
+  description: {
+    marginBottom: theme.spacing(3),
+  },
+  price: {
+    fontSize: '1.5rem',
+    fontWeight: 500,
+    marginBottom: theme.spacing(10),
+  },
+  buttonsWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > button:first-of-type': {
+      marginBottom: theme.spacing(1),
+    },
+  },
+}));
 
 const ProductDetails = () => {
+  const classes = useStyles();
   const dispatch = useAppDispatch();
 
   const { productId } = useParams();
@@ -12,7 +69,58 @@ const ProductDetails = () => {
   }, [productId]);
 
   const { currentProduct } = useAppSelector((state) => state.productReducer);
-  return <div>{currentProduct?.title}</div>;
+  return (
+    <>
+      <Header />
+      <Toolbar />
+      <Box className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Box className={classes.imageWrapper}>
+              <img
+                className={classes.image}
+                src={currentProduct?.imageUrl}
+                alt={currentProduct?.title}
+              ></img>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box className={classes.contentWrapper}>
+              <Box>
+                <Typography className={classes.title} variant='h3' gutterBottom>
+                  {currentProduct?.title}
+                </Typography>
+                <Typography className={classes.description} variant='body1' gutterBottom>
+                  {currentProduct?.description}
+                </Typography>
+                <Typography className={classes.price} variant='h5' gutterBottom>
+                  Product price: €{currentProduct?.price}.99
+                </Typography>
+              </Box>
+              <Box className={classes.buttonsWrapper}>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  size='large'
+                  fullWidth
+                >
+                  Add to Cart
+                </Button>
+                <Button
+                  variant='outlined'
+                  color='primary'
+                  size='large'
+                  fullWidth
+                >
+                  Add to Favorites
+                </Button>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default ProductDetails;
