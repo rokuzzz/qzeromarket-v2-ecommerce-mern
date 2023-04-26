14e094dae810a0853d31929ec70626cb35f1758b
Roman Kuzero <rombeldan@gmail.com>
2023-04-27 00:44:49 +0300

    feat (ProductDetails.tsx, ProductDetailsSkeleton.tsx): Add skeleton component for product details page - Create a skeleton component that mimics the structure of the ProductDetails page - Dynamically adjust the number and width of Skeleton components based on the screen size - Fix the issue where the image skeleton disappeared on small screens - Update the ProductDetails page to use the new skeleton component - Add appropriate media query breakpoints to handle different screen sizes.

--- client/src/components/products/ProductDetailsSkeleton.tsx
+++ client/src/components/products/ProductDetailsSkeleton.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Grid, Skeleton, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  Skeleton,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
 import { ClassNameMap } from '@mui/material/styles';
 
 interface ProductDetailsSkeletonProps {
@@ -18,37 +25,79 @@ interface ProductDetailsSkeletonProps {
 
 const ProductDetailsSkeleton = ({ useStyles }: ProductDetailsSkeletonProps) => {
   const classes = useStyles();
+
+  // Get the current screen size using the `useMediaQuery` hook
+  const isXXSScreen = useMediaQuery('(max-width:390px)');
+  const isSmallScreen = useMediaQuery('(max-width:900px)');
+  const isMediumScreen = useMediaQuery(
+    '(min-width:901px) and (max-width:1200px)'
+  );
+  const isLargeScreen = useMediaQuery('(min-width:1201px)');
+
+  // Set the number of Skeleton components based on the screen size
+  let skeletonCount = 0;
+  if (isSmallScreen) {
+    skeletonCount = 14;
+  } else if (isMediumScreen) {
+    skeletonCount = 10;
+  } else if (isLargeScreen) {
+    skeletonCount = 6;
+  }
+
+  // Render the Skeleton components based on the `skeletonCount`
+  const typographyRowsSkeleton = Array.from({ length: skeletonCount }).map(
+    (_, index) => (
+      <Skeleton
+        key={index}
+        variant='text'
+        animation='wave'
+        width={`${Math.floor(Math.random() * 21) + 80}%`}
+      />
+    )
+  );
+
   return (
     <Box className={classes.root}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
-          <Skeleton variant='rectangular' animation='wave' height='100%' />
+          <Box
+            sx={{
+              paddingBottom: isSmallScreen ? '100%' : undefined,
+              position: 'relative',
+            }}
+          >
+            <Skeleton
+              variant='rectangular'
+              animation='wave'
+              height={isLargeScreen ? '88vh' : '100%'}
+              width={'100%'}
+              sx={{ position: 'absolute', top: 0, left: 0 }}
+            />
+          </Box>
         </Grid>
         <Grid item xs={12} md={6}>
           <Box className={classes.contentWrapper}>
             <Box>
               <Typography className={classes.title} variant='h3' gutterBottom>
-                <Skeleton variant='text' animation='wave' />
+                {isXXSScreen ? (
+                  <>
+                    <Skeleton variant='text' animation='wave' width={'90%'} />
+                    <Skeleton variant='text' animation='wave' width={'60%'} />
+                  </>
+                ) : (
+                  <Skeleton variant='text' animation='wave' width={'300px'} />
+                )}
               </Typography>
               <Typography
                 className={classes.description}
                 variant='body1'
                 gutterBottom
               >
-                <Skeleton variant='text' animation='wave' width={'100%'} />
-                <Skeleton variant='text' animation='wave' width={'90%'} />
-                <Skeleton variant='text' animation='wave' width={'83%'} />
-                <Skeleton variant='text' animation='wave' width={'95%'} />
-                <Skeleton variant='text' animation='wave' width={'92%'} />
-                <Skeleton variant='text' animation='wave' width={'96%'} />
-                <Skeleton variant='text' animation='wave' width={'100%'} />
-                <Skeleton variant='text' animation='wave' width={'96%'} />
-                <Skeleton variant='text' animation='wave' width={'94%'} />
-                <Skeleton variant='text' animation='wave' width={'97%'} />
-                <Skeleton variant='text' animation='wave' width={'20%'} />
+                {typographyRowsSkeleton}
+                <Skeleton variant='text' animation='wave' width={'28%'} />
               </Typography>
               <Typography className={classes.price} variant='h5' gutterBottom>
-                <Skeleton variant='text' animation='wave' width={'48%'} />
+                <Skeleton variant='text' animation='wave' width={'220px'} />
               </Typography>
             </Box>
             <Box className={classes.buttonsWrapper}>
