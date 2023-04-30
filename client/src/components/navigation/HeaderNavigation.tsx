2ac54dbea06110c598e2acb82cf2e1fb6962b14b
Roman Kuzero <rombeldan@gmail.com>
2023-04-30 19:56:56 +0300

    fix: Implement AppLogo color change on cursor hover in application header
    - Add state to track cursor hover over AppLogo component
    - Change AppLogo fill color to secondary.main when cursor is hovering
      over it

--- client/src/components/navigation/HeaderNavigation.tsx
+++ client/src/components/navigation/HeaderNavigation.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Toolbar, IconButton } from '@mui/material';
+import { Theme, useTheme } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 
 import { ReactComponent as AppLogo } from '../../assets/qzeromarket-logo.svg';
 
 const HeaderNavigation = () => {
+  const theme: Theme = useTheme();
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
   return (
-    <Toolbar>
-      <IconButton
+    <Toolbar sx={{ m: 0, p: 0 }}>
+      {/* <IconButton
         size='large'
         edge='start'
         color='inherit'
@@ -14,8 +24,22 @@ const HeaderNavigation = () => {
         sx={{ pr: 0 }}
       >
         <MenuIcon />
-      </IconButton>
-      <AppLogo />
+      </IconButton> */}
+      <Link
+        to={'/'}
+        style={{ textDecoration: 'none' }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <AppLogo
+          key={new Date().getTime()}
+          fill={
+            isHovered
+              ? theme.palette.secondary.main
+              : theme.palette.primary.contrastText
+          }
+        />
+      </Link>
     </Toolbar>
   );
 };
