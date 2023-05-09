54b8858ce14e8b38f1f63704eaecb4429d038e9d
Roman Kuzero <rombeldan@gmail.com>
2023-05-09 15:45:38 +0300

    refact: Improve readability and modularity of CartDrawer component
    - Extract CartHeading into a separate component for better code organization
    - Create getCartTotalQuantity function to calculate the total cart quantity
    - Update comments to improve code documentation.

--- client/src/components/cart/CartDrawer.tsx
+++ client/src/components/cart/CartDrawer.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
-import { Box, Divider, Drawer, IconButton, Typography } from '@mui/material';
+import { Box, Divider, Drawer } from '@mui/material';
 import { styled } from '@mui/system';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
-import CloseIcon from '@mui/icons-material/Close';
 
 import { useAppDispatch, useAppSelector } from '../../hooks/appHooks';
-import {
-  countTotalPrice,
-  getUsersShoppingCart,
-} from '../../redux/slices/cartSlice';
+import { getUsersShoppingCart } from '../../redux/slices/cartSlice';
 import CartContent from './CartContent';
+import CartHeading from './CartHeading';
 
 const CartWrapper = styled(Box)(({ theme }) => ({
   padding: '12px 32px 8px 32px',
@@ -24,10 +19,10 @@ interface CartDrawerProps {
 const CartDrawer = ({ cartIsOpen, setCartIsOpen }: CartDrawerProps) => {
   const { loggedInUser } = useAppSelector((state) => state.userReducer);
 
-  const { usersShoppingCart } = useAppSelector((state) => state.cartReducer);
-  const { id, products, totalPrice } = usersShoppingCart ?? {};
-
   const dispatch = useAppDispatch();
+
+  const { usersShoppingCart } = useAppSelector((state) => state.cartReducer);
+  const { products, totalPrice } = usersShoppingCart ?? {};
 
   useEffect(() => {
     dispatch(
@@ -38,12 +33,16 @@ const CartDrawer = ({ cartIsOpen, setCartIsOpen }: CartDrawerProps) => {
     );
   }, []);
 
-  let cartTotalQuantity = 0;
-  if (products) {
-    for (let i = 0; i < products?.length; i++) {
-      cartTotalQuantity += products[i].quantity;
+  // Computes the total quantity of items in the shopping cart
+  const getCartTotalQuantity = () => {
+    let cartTotalQuantity = 0;
+    if (products) {
+      for (let i = 0; i < products?.length; i++) {
+        cartTotalQuantity += products[i].quantity;
+      }
     }
-  }
+    return cartTotalQuantity;
+  };
 
   return (
     <Drawer
@@ -59,29 +58,11 @@ const CartDrawer = ({ cartIsOpen, setCartIsOpen }: CartDrawerProps) => {
       }}
     >
       <CartWrapper>
-        <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-          {products?.length === 0 ? (
-            <ShoppingBagOutlinedIcon sx={{ width: '32px', height: '32px' }} />
-          ) : (
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <ShoppingBagIcon sx={{ width: '32px', height: '32px' }} />
-              <Typography
-                variant='h6'
-                sx={{ marginLeft: '8px', fontWeight: 700 }}
-              >
-                {cartTotalQuantity} {cartTotalQuantity === 1 ? 'Item' : 'Items'}
-              </Typography>
-            </Box>
-          )}
-          <Box sx={{ flexGrow: 1 }} />
-          <IconButton
-            onClick={() => setCartIsOpen(false)}
-            color='inherit'
-            aria-aria-label='close'
-          >
-            <CloseIcon sx={{ width: '32px', height: '32px' }} />
-          </IconButton>
-        </Box>
+        <CartHeading
+          products={products}
+          cartTotalQuantity={getCartTotalQuantity()}
+          setCartIsOpen={setCartIsOpen}
+        />
         <Divider />
         <CartContent products={products} totalPrice={totalPrice} />
       </CartWrapper>
---
+++ client/src/components/cart/CartHeading.tsx
@@ -0,0 +1,43 @@
+import { Box, IconButton, Typography } from '@mui/material';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+
+import { ProductInCart } from '../../types/cart';
+
+interface CartHeadingProps {
+  products: ProductInCart[] | undefined;
+  cartTotalQuantity: number;
+  setCartIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CartHeading = ({
+  products,
+  cartTotalQuantity,
+  setCartIsOpen,
+}: CartHeadingProps) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+      {products?.length === 0 ? (
+        <ShoppingBagOutlinedIcon sx={{ width: '32px', height: '32px' }} />
+      ) : (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <ShoppingBagIcon sx={{ width: '32px', height: '32px' }} />
+          <Typography variant='h6' sx={{ marginLeft: '8px', fontWeight: 700 }}>
+            {cartTotalQuantity} {cartTotalQuantity === 1 ? 'Item' : 'Items'}
+          </Typography>
+        </Box>
+      )}
+      <Box sx={{ flexGrow: 1 }} />
+      <IconButton
+        onClick={() => setCartIsOpen(false)}
+        color='inherit'
+        aria-aria-label='close'
+      >
+        <CloseIcon sx={{ width: '32px', height: '32px' }} />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default CartHeading;
