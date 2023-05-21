52e63a0164733bb29082bf55cf617898b0eb39d7
Roman Kuzero <rombeldan@gmail.com>
2023-05-21 17:06:31 +0300

    refactor: Improve authController readability and modularity
    - Encapsulate password encryption into separate function, encryptPassword
    - Utilize encryptPassword function in register for encrypting user passwords
    - Forward caught exceptions in register and login functions to next middleware.

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -1,27 +1,34 @@
-import { Response, Request, NextFunction } from "express";
+import { Response, Request, NextFunction } from 'express'
 import CryptoJS from 'crypto-js'
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
 
-import { CRYPTO_SECRET, JWT_SECRET } from "../util/secrets";
-import User, { UserRole } from "../models/User";
-import { UnauthorizedError } from "../helpers/apiError";
-import authService from "../services/authService";
+import { CRYPTO_SECRET, JWT_SECRET } from '../util/secrets'
+import User, { UserRole } from '../models/User'
+import { UnauthorizedError } from '../helpers/apiError'
+import authService from '../services/authService'
+
+const encryptPassword = (password: string): string => {
+  return CryptoJS.AES.encrypt(password, CRYPTO_SECRET).toString()
+}
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
-  const {firstname, lastname, username, email } = req.body
-  const password = CryptoJS.AES.encrypt(req.body.password, CRYPTO_SECRET).toString()
+  const { firstname, lastname, username, email, password } = req.body
   const role: UserRole = 'guest'
-  const newUser = new User({ 
-    firstname, 
-    lastname, 
-    username, 
-    email, 
-    password,
-    role
+
+  const encryptedPassword = encryptPassword(password)
+
+  const newUser = new User({
+    firstname,
+    lastname,
+    username,
+    email,
+    password: encryptedPassword,
+    role,
   })
 
-  try{
+  try {
     const savedUser = await authService.createUser(newUser)
+
     res.status(201).json(savedUser)
   } catch (err) {
     next(err)
@@ -29,10 +36,12 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
-  try{
-    const {username, password} = req.body
-    const loginData = {username, password}
+  try {
+    const { username, password } = req.body
+    const loginData = { username, password }
+
     const user = await authService.authenticateUser(loginData)
+
     res.status(200).json(user)
   } catch (err) {
     next(err)
@@ -41,5 +50,5 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 
 export default {
   register,
-  login
+  login,
 }
