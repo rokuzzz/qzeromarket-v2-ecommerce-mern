ab16f60bbed4b232b8baebe519781d84d747f18c
Roman Kuzero <rombeldan@gmail.com>
2023-05-30 17:00:15 +0300

    test: Test the modifyFavorites reducer
    - Write the test for modifyReducer
      The test approves that item were added to favorites.

--- client/src/test/reducers/favoritesReducer.test.ts
+++ client/src/test/reducers/favoritesReducer.test.ts
@@ -1,4 +1,7 @@
-import { getUsersFavorites } from '../../redux/slices/favoritesSlice';
+import {
+  getUsersFavorites,
+  modifyFavorites,
+} from '../../redux/slices/favoritesSlice';
 import createTestStore from '../utils/testStore';
 
 let store = createTestStore();
@@ -27,5 +30,20 @@ describe('test favorites reducer', () => {
       store.getState().favoritesReducer.usersFavorites?.favoritesItems[0]
         .itemInFavorites.title
     ).toBe('Whispering Mist Wool Hoodie');
+  });
+
+  test('should modify users favorites', async () => {
+    await store.dispatch(
+      modifyFavorites({
+        title: 'Classic Black T-Shirt',
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJiZDEyZTkzNjQ2NTJlNGRlMDM3ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NTQ1MzQzNCwiZXhwIjoxNjg1NzEyNjM0fQ.oHtyPlDZWd8_8MGi4GUw_aRHuLZK8P43UTg7d3cBnMI',
+      })
+    );
+
+    expect(
+      store.getState().favoritesReducer.usersFavorites?.favoritesItems[1]
+        .itemInFavorites.title
+    ).toBe('Classic Black T-Shirt');
   });
 });
