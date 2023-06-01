8112f7372ddcb1e01be89b2428ff3d710d3fbf53
Roman Kuzero <rombeldan@gmail.com>
2023-06-01 14:31:33 +0300

    test: Add unit test for getAllFavorites reducer in FavoritesSlice
    - Implement test for the getAllFavorites reducer that dispatches the getAllFavorites action and asserts the length of data in the allFavorites state
    - Utilize a hardcoded token for testing purposes.

--- client/src/test/reducers/favoritesReducer.test.ts
+++ client/src/test/reducers/favoritesReducer.test.ts
@@ -1,4 +1,5 @@
 import {
+  getAllFavorites,
   getUsersFavorites,
   modifyFavorites,
 } from '../../redux/slices/favoritesSlice';
@@ -16,7 +17,7 @@ describe('test favorites reducer', () => {
       getUsersFavorites({
         userId: '63dbbd12e9364652e4de037f',
         token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJiZDEyZTkzNjQ2NTJlNGRlMDM3ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NTM1ODQ2MSwiZXhwIjoxNjg1NjE3NjYxfQ.2noGCa7irc_hHEm2uc7hUyWSradzble5AIhSw8J5_xE',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJiZDEyZTkzNjQ2NTJlNGRlMDM3ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NTU1MjQ4OSwiZXhwIjoxNjg1ODExNjg5fQ.huSkE5YKfmQlUvGkZVWZ5y-gjONNjaPyK0-HAwuzsR0',
       })
     );
 
@@ -32,18 +33,37 @@ describe('test favorites reducer', () => {
     ).toBe('Whispering Mist Wool Hoodie');
   });
 
-  test('should modify users favorites', async () => {
+  // test('should modify users favorites', async () => {
+  //   await store.dispatch(
+  //     modifyFavorites({
+  //       title: 'Classic Black T-Shirt',
+  //       token:
+  //         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJiZDEyZTkzNjQ2NTJlNGRlMDM3ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NTU1MjQ4OSwiZXhwIjoxNjg1ODExNjg5fQ.huSkE5YKfmQlUvGkZVWZ5y-gjONNjaPyK0-HAwuzsR0',
+  //     })
+  //   );
+
+  //   // console.log(
+  //   //   'first item in favorites title: ',
+  //   //   store.getState().favoritesReducer.usersFavorites?.data.favoritesItems[0]
+  //   //     .itemInFavorites.title
+  //   // );
+
+  //   expect(
+  //     store.getState().favoritesReducer.usersFavorites?.data.favoritesItems[1]
+  //       .itemInFavorites.title
+  //   ).toBe('Classic Black T-Shirt');
+  // });
+
+  test('should get all favorites', async () => {
     await store.dispatch(
-      modifyFavorites({
-        title: 'Classic Black T-Shirt',
+      getAllFavorites({
         token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJiZDEyZTkzNjQ2NTJlNGRlMDM3ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NTQ1MzQzNCwiZXhwIjoxNjg1NzEyNjM0fQ.oHtyPlDZWd8_8MGi4GUw_aRHuLZK8P43UTg7d3cBnMI',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGJiZDEyZTkzNjQ2NTJlNGRlMDM3ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NTYxNTQ5OSwiZXhwIjoxNjg1ODc0Njk5fQ.CjVvXxM9FlyD-yxLO-31bwASFUKIYUIwpa_uYGxoS9A',
       })
     );
 
     expect(
-      store.getState().favoritesReducer.usersFavorites?.data.favoritesItems[1]
-        .itemInFavorites.title
-    ).toBe('Classic Black T-Shirt');
+      store.getState().favoritesReducer.allFavorites?.data.length
+    ).toBeGreaterThan(0);
   });
 });
