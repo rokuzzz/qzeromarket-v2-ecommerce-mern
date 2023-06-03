c86cb12123db9de4efef939f68027027b022d5f9
Roman Kuzero <rombeldan@gmail.com>
2023-06-03 23:56:43 +0300

    feat: Continue building FavoritesList component
    - Create the basic structure of the FavoritesList component
    - Include the FavoritesList component in the Favorites page.

--- client/src/components/favorites/FavoritesList.tsx
+++ client/src/components/favorites/FavoritesList.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import styled from '@mui/material/styles/styled';
+import { Box, Typography } from '@mui/material';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  heading: {
+    margin: '20px',
+  },
+  headingText: {
+    fontWeight: 700,
+    opacity: '80%',
+  },
+};
+
+const FavoritesListWrapper = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    margin: '0 0px',
+  },
+  [theme.breakpoints.up('md')]: {
+    margin: '0 48px',
+  },
+}));
 
 const FavoritesList = () => {
   return (
-    <div>FavoritesList</div>
-  )
-}
+    <FavoritesListWrapper sx={styles.root}>
+      <Box sx={styles.heading}>
+        <Typography variant='h5' sx={styles.headingText}>
+          Favorites
+        </Typography>
+      </Box>
+    </FavoritesListWrapper>
+  );
+};
 
-export default FavoritesList
+export default FavoritesList;
--- client/src/pages/Favorites.tsx
+++ client/src/pages/Favorites.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import { Toolbar } from '@mui/material';
 
 import Header from '../components/navigation/Header';
+import FavoritesList from '../components/favorites/FavoritesList';
 
 const Favorites = () => {
   return (
     <>
       <Header />
       <Toolbar sx={{ height: '60px' }} />
+      <FavoritesList />
     </>
   );
 };
