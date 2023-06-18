a18d2d62504c04b01ed75022216d7045f6f598a1
Roman Kuzero <rombeldan@gmail.com>
2023-06-18 18:36:21 +0300

    Feat: Introduce horizontal scrolling product list
    - Implement functionality to fetch and display bestsellers in a horizontal scrolling list
    - Include an image, title, and favorite icon for each product in the list
       - Note: favorite icon is currently static and does not have functionality
       - The list design is not yet fully responsive and needs further refinement

--- client/src/components/favorites/HorizontalRecommendationList.tsx
+++ client/src/components/favorites/HorizontalRecommendationList.tsx
@@ -1,70 +1,28 @@
+import { useEffect } from 'react';
 import {
   Box,
-  Card,
   CardActionArea,
-  CardContent,
-  CardMedia,
-  Grid,
-  Skeleton,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
   Typography,
 } from '@mui/material';
+import styled from '@mui/material/styles/styled';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
 import { useAppDispatch, useAppSelector } from '../../hooks/common/appHooks';
-import { useEffect } from 'react';
 import { fetchAllProducts } from '../../redux/slices/productSlice';
 import { Link } from 'react-router-dom';
 
-const styles = {
-  root: {
-    flexGrow: 1,
+const FavoritesHorizontalListWrapper = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    margin: '0 16px 32px 16px',
   },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    borderRadius: '0px',
-    boxShadow: 'none',
+  [theme.breakpoints.up('md')]: {
+    margin: '0 24px 48px 24px',
   },
-  link: {
-    textDecoration: 'none',
-    outline: 'none',
-    color: 'inherit',
-    '&:focus, &:hover, &:visited, &:link, &:active': {
-      color: 'inherit',
-    },
-  },
-  cardContent: {
-    padding: '16px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
-  media: {
-    height: 0,
-    width: '100%',
-    paddingTop: '100%', // 1:1 aspect ratio
-  },
-  heading: {
-    margin: '20px',
-  },
-  headingText: {
-    fontWeight: 700,
-    opacity: '80%',
-  },
-  title: {
-    fontSize: '0.9rem',
-    lineHeight: 1.2,
-    margin: '8px 0 0',
-    whiteSpace: 'nowrap',
-  },
-  price: {
-    fontSize: '1rem',
-    fontWeight: 900,
-    lineHeight: 1.2,
-    opacity: '80%',
-    margin: '4px 0 32px',
-  },
-};
+}));
 
 const HorizontalRecommendationList = () => {
   const dispatch = useAppDispatch();
@@ -82,52 +40,63 @@ const HorizontalRecommendationList = () => {
   }, []);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        overflowX: 'auto',
-        flexDirection: 'row',
-        scrollbarWidth: 'none', // for Firefox
-        '&::-webkit-scrollbar': {
-          display: 'none', // for Chrome, Safari, and Opera
-        },
-      }}
-    >
-      <Grid container spacing={2} direction='row'>
+    <FavoritesHorizontalListWrapper>
+      <Typography variant='h5' sx={{ fontWeight: 700, opacity: '80%' }}>
+        Find Your Next Favorite
+      </Typography>
+      <ImageList
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          overflowX: 'scroll',
+          scrollbarWidth: 'none', // For Firefox
+          '&::-webkit-scrollbar': {
+            display: 'none', // For Chrome, Safari and Opera
+          },
+        }}
+      >
         {data.map((item, index) => (
-          <Grid
-            item
+          <ImageListItem
             key={index}
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-            xl={2}
-            sx={{ minWidth: 200, flexShrink: 0 }}
+            sx={{
+              flex: '0 0 auto',
+              margin: 0.5,
+              position: 'relative',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '27%',
+            }}
           >
-            <Card sx={styles.card}>
-              <Link style={styles.link} to={`/products/${item._id}`}>
-                <CardActionArea>
-                  <CardMedia
-                    sx={styles.media}
-                    image={item.imageUrl}
-                    title={item.title}
-                  />
-                  <CardContent sx={styles.cardContent}>
-                    <Typography variant='subtitle1' sx={styles.title}>
-                      {item.title}
-                    </Typography>
-                    <Typography variant='overline' sx={styles.price}>
-                      €{item.price}.00
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Link>
-            </Card>
-          </Grid>
+            <Link to={`/products/${item._id}`}>
+              <img
+                src={item.imageUrl}
+                alt={item.title}
+                style={{ width: '100%' }}
+              />
+            </Link>
+            <ImageListItemBar
+              sx={{
+                background:
+                  'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                  'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+              }}
+              title={item.title}
+              position='top'
+              actionIcon={
+                <IconButton
+                  sx={{ color: 'white' }}
+                  aria-label={`star ${item.title}`}
+                >
+                  <FavoriteBorderIcon />
+                </IconButton>
+              }
+              actionPosition='left'
+            />
+          </ImageListItem>
         ))}
-      </Grid>
-    </Box>
+      </ImageList>
+    </FavoritesHorizontalListWrapper>
   );
 };
 
