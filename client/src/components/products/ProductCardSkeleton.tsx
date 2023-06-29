705675f76788058473dd96f5efbe0e9dba550e52
Roman Kuzero <rombeldan@gmail.com>
2023-06-29 23:33:34 +0300

    Update: Enhance ProductCardSkeleton with Dynamic Title Width
    - Implemented a dynamic skeleton title width using a randomized value between 100 and 230
    - Reduced the width of the price skeleton for a better visual appeal

--- client/src/components/products/ProductCardSkeleton.tsx
+++ client/src/components/products/ProductCardSkeleton.tsx
@@ -1,8 +1,4 @@
-import {
-  Card,
-  CardActionArea,
-  Skeleton,
-} from '@mui/material';
+import { Card, CardActionArea, Skeleton } from '@mui/material';
 import {
   ProductDetailsCardContent,
   ProductPriceTypography,
@@ -14,16 +10,22 @@ interface ProductCardSkeletonProps {
 }
 
 const ProductCardSkeleton = ({ styles }: ProductCardSkeletonProps) => {
+  const randSkeletonTitleWidth = Math.floor(Math.random() * 130) + 100;
+
   return (
     <Card sx={styles.card}>
       <CardActionArea>
         <Skeleton sx={styles.media} variant='rectangular' animation='wave' />
         <ProductDetailsCardContent>
           <ProductTitleTypography variant='subtitle1'>
-            <Skeleton width={'150px'} variant='text' animation='wave' />
+            <Skeleton
+              width={randSkeletonTitleWidth}
+              variant='text'
+              animation='wave'
+            />
           </ProductTitleTypography>
           <ProductPriceTypography variant='overline'>
-            <Skeleton width={'80px'} variant='text' animation='wave' />
+            <Skeleton width={'65px'} variant='text' animation='wave' />
           </ProductPriceTypography>
         </ProductDetailsCardContent>
       </CardActionArea>
